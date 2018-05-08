e4b463eb41a1168eeceab538701c56f1160a4336
maynardz <maynard.zach23@gmail.com>
2018-05-08 11:08:39 -0400

    ZM added account management component

---
+++ src/app/components/acc-management/acc-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acc-management',
+  templateUrl: './acc-management.component.html',
+  styleUrls: ['./acc-management.component.scss']
+})
+export class AccManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
