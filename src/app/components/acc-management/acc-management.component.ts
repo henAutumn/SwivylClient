d69ec338e45c57be785c864186d55ef0774cebb7
henAutumn <33758290+henAutumn@users.noreply.github.com>
2018-05-09 11:44:16 -0400

    ah added modal

--- src/app/components/acc-management/acc-management.component.ts
+++ src/app/components/acc-management/acc-management.component.ts
@@ -7,13 +7,15 @@ import { AccManagementService } from '../../Services/acc-management.service';
   selector: 'app-acc-management',
   templateUrl: './acc-management.component.html',
   styleUrls: ['./acc-management.component.scss']
+  
 })
 export class AccManagementComponent implements OnInit {
 
-  constructor(private _accmanagementservice: AccManagementService) { }
+  constructor(
+    private _accmanagementservice: AccManagementService) { }
 
   ngOnInit() {
-    this.newUser();
+this.newUser();
   }
 
   newUser(){
