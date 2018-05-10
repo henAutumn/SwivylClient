9af08c4952476fd0eb7c988b2365477b9a5fe9ab
maynardz <maynard.zach23@gmail.com>
2018-05-10 09:26:05 -0400

    ZM login service

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -6,7 +6,8 @@ const login = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
     user {
-      name
+      firstName
+      lastName
       id
     }
     token
