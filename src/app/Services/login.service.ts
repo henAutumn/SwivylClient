38410d752d3dafaa52ef04312159628062fe3ea7
henAutumn <33758290+henAutumn@users.noreply.github.com>
2018-05-10 09:18:30 -0400

    ah added more fields for user

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -6,7 +6,7 @@ const login = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
     user {
-      name
+      firstName
       id
     }
     token
