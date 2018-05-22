4f231cd3362c3aeec6aa58a51df4ae1fe22d15b9
maynardz <maynard.zach23@gmail.com>
2018-05-22 09:24:01 -0400

    ZM firebase

--- src/app/views/policy-corner/policy-corner.module.ts
+++ src/app/views/policy-corner/policy-corner.module.ts
@@ -3,13 +3,5 @@ import { NgModule } from '@angular/core';
 import { PolicyCornerComponent } from './policy-corner.component';
 
 
-@NgModule({
-  imports: [
-   
-  ],
-  declarations: [ PolicyCornerComponent],
-  providers: [
-  
-  ]
-})
+
 export class PolicyCornerModule { }
